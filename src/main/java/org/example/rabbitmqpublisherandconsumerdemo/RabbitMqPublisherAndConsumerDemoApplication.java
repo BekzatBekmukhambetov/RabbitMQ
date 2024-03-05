15e2858a4d0ec430e55c0379dbc6bc125715db9c
Bekzat <bekzatxxxbeka@gmail.com>
2024-03-05 11:59:29 +0100

    first commit

---
+++ src/main/java/org/example/rabbitmqpublisherandconsumerdemo/RabbitMqPublisherAndConsumerDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.rabbitmqpublisherandconsumerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqPublisherAndConsumerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqPublisherAndConsumerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/rabbitmqpublisherandconsumerdemo/RabbitMqPublisherAndConsumerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rabbitmqpublisherandconsumerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqPublisherAndConsumerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
